9b865c6cc471a811e5e69acc80546bea3987b149
AWS <noreply-aws@amazon.com>
2023-02-25 17:54:20 +0000

    Initial commit made by AWS during repository creation.

---
+++ AspNetCoreWebService/Controllers/Response.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebService.Controllers
+{
+    public class Response
+    {
+        public Response(string o)
+        {
+            Output = o;
+        }
+
+        public string Output
+        {
+            get;
+            private set;
+        }
+    }
+}
